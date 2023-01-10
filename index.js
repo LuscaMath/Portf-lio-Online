978480d7c54aad2fdc57d62e7acb612d1be57370
Lucas Matheus da Silva Peres <83469680+LuscaMath@users.noreply.github.com>
2023-01-10 12:05:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const main = document.getElementById('main')
 const slider = document.querySelector('.slider')
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
+const mainText = document.querySelector('main-section-text')
+const projectDiv = document.querySelectorAll('.projects-item')
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
@@ -20,14 +22,20 @@ document.querySelectorAll('.nav-link').forEach(n => {
 
 toogle.addEventListener('click', function() {
     if (main.dataset.theme === 'dark') {
-        root.style.setProperty('--primary-color', '#656DC8')
-        root.style.setProperty('--secondary-color', '#ffffff')
+        root.style.setProperty('--primary-color', '#ffffff')
+        root.style.setProperty('--secondary-color', '#6C63FF')
+        root.style.setProperty('--font-color', '#242424')
+        root.style.setProperty('--terciary-color', 'lightgrey')
+        root.style.setProperty('--form-color', 'rgb(173, 166, 166)')
         slider.setAttribute('src', './imagens/sol.png');
         slider.style.backgroundColor = 'yellow'
         main.dataset.theme = 'light'
     } else {
-        root.style.setProperty('--primary-color', '#2c2c2c')
-        root.style.setProperty('--secondary-color', '#ffffff')
+        root.style.setProperty('--primary-color', '#171818')
+        root.style.setProperty('--secondary-color', '#6C63FF')
+        root.style.setProperty('--font-color', '#ffffff')
+        root.style.setProperty('--terciary-color', '#282c2c')
+        root.style.setProperty('--form-color', '#171818')
         slider.setAttribute('src', './imagens/lua.png');
         slider.style.backgroundColor = '#48b4e0'
         main.dataset.theme = 'dark'
